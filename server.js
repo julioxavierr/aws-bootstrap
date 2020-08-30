2d79a9aa460cb286499523abc5d9462e29c02996
Júlio Xavier <julioxavierr@gmail.com>
2020-08-30 20:36:48 -0300

    add hostname to greeting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require("os");
 const http = require("http");
 
-const message = "Hello Cloud\n";
+const message = `Hello World from ${hostname()}\n`;
 const port = 8080;
 
 const server = http.createServer((req, res) => {
